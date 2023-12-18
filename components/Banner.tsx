b75bc1af71bf509872b7d9e2aef24b52b50ba674
Ibrahim <isye6924@uni.sydney.edu.au>
2023-12-18 22:48:43 +1100

    fixed horizontal scroll issue

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -21,7 +21,7 @@ function Banner({ netflixOriginals }: Props) {
   return (
     //changed lg:h-[65vh] to lg:h-[85vh]
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[85vh] lg:justify-end lg:pb-12">
-      <div className="absolute top-0 left-0 h-[95vh] -z-10 w-screen">
+      <div className={`absolute top-0 left-0 h-[95vh] -z-10 banner-width`}>
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
